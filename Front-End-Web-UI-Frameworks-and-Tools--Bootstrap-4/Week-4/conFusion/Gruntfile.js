6dd3272828fd65d60985be7f9f7c897e309d9876
Victor Alonso <vacontrerasmeza@gmail.com>
2018-07-17 21:49:57 -0500

    Gruntfile correction

--- Front-End-Web-UI-Frameworks-and-Tools--Bootstrap-4/Week-4/conFusion/Gruntfile.js
+++ Front-End-Web-UI-Frameworks-and-Tools--Bootstrap-4/Week-4/conFusion/Gruntfile.js
@@ -42,18 +42,22 @@ module.exports = function (grunt) {
             }
         },
         
-        copy:{
+        copy: {
             html: {
-                files: [{
+                files: [
+                {
+                    //for html
                     expand: true,
                     dot: true,
                     cwd: './',
                     src: ['*.html'],
                     dest: 'dist'
-                }]
+                }]                
             },
             fonts: {
-                files: [{
+                files: [
+                {
+                    //for font-awesome
                     expand: true,
                     dot: true,
                     cwd: 'node_modules/font-awesome',
@@ -65,18 +69,17 @@ module.exports = function (grunt) {
 
         clean: {
             build: {
-                src: ['dist/']
+                src: [ 'dist/']
             }
         },
 
         imagemin: {
             dynamic: {
                 files: [{
-                    expand: true,
-                    dot: true,
-                    cwd: './',
-                    src: ['img/*.{png,jpg,gif}'],
-                    dest: 'dist/'
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: './',                   // Src matches are relative to this path
+                    src: ['img/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'dist/'                  // Destination path prefix
                 }]
             }
         },
@@ -84,39 +87,42 @@ module.exports = function (grunt) {
         useminPrepare: {
             foo: {
                 dest: 'dist',
-                src: ['contactus.html', 'aboutus.html', 'index.html']
+                src: ['contactus.html','aboutus.html','index.html']
             },
             options: {
                 flow: {
-                    flow: {
-                        steps: {
-                            css: ['cssmin'],
-                            js: ['uglify']
-                        },
-                        post: {
-                            css: [{
-                                name: 'cssmin',
-                                createConfig: function (context, block){
-                                    var generated = context.options.generated;
-                                    generated.options = {
-                                        keepSpecialComments: 0, rebase: false
-                                    };
-                                }
-                            }]
-                        }
+                    steps: {
+                        css: ['cssmin'],
+                        js:['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block) {
+                            var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }       
+                        }]
                     }
                 }
             }
         },
 
+        // Concat
         concat: {
             options: {
                 separator: ';'
             },
+  
+            // dist configuration is provided by useminPrepare
             dist: {}
         },
 
+        // Uglify
         uglify: {
+            // dist configuration is provided by useminPrepare
             dist: {}
         },
 
@@ -124,13 +130,17 @@ module.exports = function (grunt) {
             dist: {}
         },
 
+        // Filerev
         filerev: {
             options: {
                 encoding: 'utf8',
                 algorithm: 'md5',
                 length: 20
             },
+  
             release: {
+            // filerev:release hashes(md5) all assets (images, js and css )
+            // in dist directory
                 files: [{
                     src: [
                         'dist/js/*.js',
@@ -139,23 +149,27 @@ module.exports = function (grunt) {
                 }]
             }
         },
-
+  
+        // Usemin
+        // Replaces all assets with their revved version in html and css files.
+        // options.assetDirs contains the directories for finding the assets
+        // according to their relative paths
         usemin: {
-            html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/index.html'],
+            html: ['dist/contactus.html','dist/aboutus.html','dist/index.html'],
             options: {
-                assetsDirs: ['dist', 'dist/css', 'dist/js']
+                assetsDirs: ['dist', 'dist/css','dist/js']
             }
         },
 
-        htmlmin: {
-            dist: {
-                options: {
+        htmlmin: {                                         // Task
+            dist: {                                        // Target
+                options: {                                 // Target options
                     collapseWhitespace: true
                 },
-                files: {
-                    'dist/index.html': 'dist/index.html',
+                files: {                                   // Dictionary of files
+                    'dist/index.html': 'dist/index.html',  // 'destination': 'source'
                     'dist/contactus.html': 'dist/contactus.html',
-                    'dist/aboutus.html': 'dist/aboutus.html'
+                    'dist/aboutus.html': 'dist/aboutus.html',
                 }
             }
         }
